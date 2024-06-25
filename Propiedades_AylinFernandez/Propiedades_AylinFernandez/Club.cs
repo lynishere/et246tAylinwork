f638021ec08a2f3a2efbf85e8cbaabd5b402035b
aylin <127500980+lynishere@users.noreply.github.com>
2024-06-25 20:32:39 -0300

    actividad 1 propiedades con nazarena

---
+++ Propiedades_AylinFernandez/Propiedades_AylinFernandez/Club.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propiedades_AylinFernandez
+{
+     class Club
+    {
+        private Socios socio1,socio2,socio3;
+        public Club()
+        {
+            socio1 = new Socios();
+            socio1.Nombre = "Pedrito"; // para pedir el nombre tambien se puede usar console.writeline("ingrese nombre"); socio.nombre=console.readline();
+            socio1.Antiguiedad = 19;
+            socio2 = new Socios();
+            socio2.Nombre = "Juana";
+            socio2.Antiguiedad = 20;
+            socio3 = new Socios();
+            socio3.Nombre = "Lurdes";
+            socio3.Antiguiedad = 20;
+        }
+        /*  public void Ingresar() // esto se puede eliminar tranquilamente y lo podes poner en donde esta =0
+          {
+              socio1.Antiguiedad = socio1.Antiguiedad + 20;
+              socio2.Antiguiedad = socio2.Antiguiedad + 15;
+              socio3.Antiguiedad = socio3.Antiguiedad + 19;
+          }*/
+        public void Imprimir()
+        {
+            if (socio2.Antiguiedad> socio1.Antiguiedad & socio2.Antiguiedad > socio3.Antiguiedad)
+            {
+                Console.WriteLine("El socio con mas antiguedad es: " + socio2.Nombre);
+            }else if(socio3.Antiguiedad > socio1.Antiguiedad & socio3.Antiguiedad > socio2.Antiguiedad)
+            {
+                Console.WriteLine("El socio con mas antiguedad es: " + socio3.Nombre);
+            }
+            else
+            {
+                Console.WriteLine("El socio con mas antiguedad es: " + socio1.Nombre);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Club Clubes = new Club();
+            Clubes.Ingresar();
+            Clubes.Imprimir();
+            Console.ReadKey();
+        }
+    }
+    
+} 
---
+++ Propiedades_AylinFernandez/Propiedades_AylinFernandez/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Propiedades_AylinFernandez
+{
+     class Socios
+    {
+        private string nombre;
+        private int antiguiedad;
+        public string Nombre
+        {
+            set { nombre = value; }
+            get { return nombre; }
+        }
+        public int Antiguiedad
+        {
+            set { antiguiedad = value; }
+            get { return  antiguiedad; }
+        }
+     
+    }
+public socio(){
+Nombre=nombre;
+Antiguiedad=antiguiedad;
+}
+}
